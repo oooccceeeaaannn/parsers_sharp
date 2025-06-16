5cf1aca3869c3ad8034e8af4c02e2358ce71f1fd
oooccceeeaaannn <ocean18071@qq.com>
2025-06-16 16:05:27 +0800

    fixed REFERS

--- Lua/conditions.lua
+++ Lua/conditions.lua
@@ -1968,29 +1968,33 @@ condlist.powered = function(params,checkedconds,checkedconds_,cdata) --To correc
 	
 condlist['refers'] = function(params,checkedconds,checkedconds_,cdata)
 	for i, j in pairs(params) do
+		local passedthischeck = false
 		local _params = j
 
 		local unitname = mmf.newObject(cdata.unitid).strings[UNITNAME]
 
 		if (string.sub(unitname, 1, 5) == "text_") or (string.sub(unitname, 1, 5) == "node_") then
 			if (string.sub(unitname, 6) == _params) then
-				return true, checkedconds
+				passedthischeck = true
 			end
 		elseif (string.sub(unitname, 1, 6) == "glyph_") or (string.sub(unitname, 1, 6) == "event_") or (string.sub(unitname, 1, 6) == "orbit_") or (string.sub(unitname, 1, 6) == "logic_") then
 			if (string.sub(unitname, 7) == _params) then
-				return true, checkedconds
+				passedthischeck = true
 			end
 		end
 
 		if hasfeature(unitname, "is", "word", cdata.unitid) and (unitname == _params) then
-			return true, checkedconds
+			passedthischeck = true
 		elseif hasfeature(unitname, "is", "symbol", cdata.unitid) and (unitname == _params) then
-			return true, checkedconds
+			passedthischeck = true
 		elseif hasfeature(unitname, "is", "flow", cdata.unitid) and (unitname == _params) then
-			return true, checkedconds
+			passedthischeck = true
 		elseif hasfeature(unitname, "is", "center", cdata.unitid) and (unitname == _params) then
-			return true, checkedconds
+			passedthischeck = true
+		end
+		if not passedthischeck then
+			return false, checkedconds
 		end
 	end
-	return false, checkedconds
+	return true, checkedconds
 end
